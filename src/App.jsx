9bbddd6836cb9aa477bd1e2821def6bac712986c
Shivani Sharma <shivani1406sharma@gmail.com>
2021-05-10 23:01:23 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+import React from "react";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
